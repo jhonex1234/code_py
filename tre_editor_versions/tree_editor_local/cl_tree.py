4f7ea0ad638c9c458ac56db9722fe22505d31b63
jhonex1234 <jajoya17@misena.edu.co>
2018-03-20 15:23:41 -0500

    se almacena backup tree editor

--- tre_editor_versions/tree_editor_local/cl_tree.py
+++ tre_editor_versions/tree_editor_local/cl_tree.py
@@ -10,15 +10,23 @@ class clTree():
     
     def __init__(self,namefile):
         self.namefile = namefile
-        self.route = '/home/jhonex/Documentos/codigo/codigo_py_soport/tree_editor/resource_bd/bd_script/resource_dbtree.sql'
-        self.insertItem('')
-        self.valdata('')
+        self.route = '/home/jhonex/Documentos/codigo/codigo_py_soport/tre_editor_versions/tree_editor_local/resource_bd/bd_script/resource_dbtree.sql'
+        self.tree_config = {'initial_state': '','templates': [],'max_attempts': 0,
+            'topics':{'pendiente':'pendiente'},'transitions':[],'models': [],'final_state' : ''}
+        self.insertItemTemplate('')
+        self.validateStatedata('')
         self.makeListPkl('')
-    #make state
-    def makeState(self,state):
+        self.searchmodel('','')
+        self.validateTemplate('','','','')
+        self.validatetrasition('','','','')
+        
+    def getTreeConfig(self):
+        return self.tree_config
+    
+    def makeListState(self,state):
         if(self.namefile != '' and state != ''):
             msj = ''
-            if(self.valdata(state)):
+            if(self.validateStatedata(state)):
                 msj = '!La clave no se pudo crear ya exite¡'
             else:
                 cn = db.connect(self.route)
@@ -28,8 +36,9 @@ class clTree():
                 cur.close()
                 msj = '!clave creada¡'
         return msj   
-    #attend method makestate
-    def valdata(self,state):
+    
+    def validateStatedata(self,state):
+        li_1 = []
         cn = db.connect(self.route)
         cur = cn.cursor()
         cur.execute('SELECT keylistState FROM tbl_listState where nameFile="%s" and keylistState="%s"'%(self.namefile,state))
@@ -37,8 +46,9 @@ class clTree():
         cn.commit()
         cur.close()
         return sc_list
-    #return list items
+    
     def getState(self):
+        li_1 = []
         cn = db.connect(self.route)
         cur = cn.cursor()
         cur.execute("SELECT keylistState FROM tbl_listState where nameFile='%s'"%(self.namefile))
@@ -46,11 +56,14 @@ class clTree():
         cn.commit()
         cur.close()
         if(sc_list):
-            sc_list = sc_list
+            sc_list = [list(i) for i in sc_list]
+            for i in sc_list:
+                li_1.append(i[0])
+            sc_list = li_1
         else:
             sc_list = '!no existen estados asociados para este archivo¡'
-        return  [list(i) for i in sc_list]
-    #delete state
+        return  sc_list
+    
     def deleteState(self,state):
         cn = db.connect(self.route)
         cur = cn.cursor()
@@ -59,7 +72,7 @@ class clTree():
         cur.close()
         msj = '!Eliminado¡'
         return msj
-    #edit state 
+    
     def editState(self,state,modState):
         cn = db.connect(self.route)
         cur = cn.cursor()
@@ -68,24 +81,25 @@ class clTree():
         cur.close()
         msj = '!Editado¡'
         return msj
-    #load and make list item of file type json
+    
     def makeListTemplates(self,fileTemplate):
         if(fileTemplate):   
             try:
                 file =  json.load(open(fileTemplate,'r'))
                 file = list(file.keys())
-                msj = self.insertItem(file)
+                msj = self.insertItemTemplate(file)
             except OSError:
                 msj = '!No se pudo cargar archivo¡'
         else:
             msj = '!Ingresa un Archivo¡'
         return msj
-    #method suport makeListTemplates
-    def insertItem(self,lis):
+    
+    def insertItemTemplate(self,lis):
         if(lis):
             for row in lis:
                 cn = db.connect(self.route)
                 cur = cn.cursor()
+                cur.execute('delete from tbl_keyslabel where nameFile="%s" and keylabel="%s"'%(self.namefile,row))
                 cur.execute('INSERT INTO tbl_keyslabel (nameFile ,keylabel) VALUES ("%s","%s")'%(self.namefile,row))
                 cn.commit()
                 cur.close()
@@ -93,8 +107,9 @@ class clTree():
         else:
             msj = ''
         return  msj
-    #return list items template
+    
     def getListKeyLabel(self):
+        li_1 = []
         cn = db.connect(self.route)
         cur = cn.cursor()
         cur.execute("SELECT keylabel FROM tbl_keyslabel where nameFile='%s'"%(self.namefile))
@@ -103,16 +118,21 @@ class clTree():
         cur.close()
         if(sc_list):
             sc_list = [list(i) for i in sc_list] 
+            for i in sc_list:
+                li_1.append(i[0])
+            
+            sc_list = li_1
         else:
             sc_list = '!no existen estados asociados para este archivo¡'
         return sc_list
-    #method simulation load of type pkl 
+    
     def makeListPkl(self,filepkl):
-        listpkl = []
         if(filepkl != ''):
-            listpkl = ['usuario','teste','nuevo']
-        return listpkl
-    #load and make list itemspkl
+            models = joblib.load(filepkl)
+        else:
+            models = ''
+        return list(models)
+    
     def loadPKL(self,namedoc):
         msj = ''
         if(namedoc):
@@ -120,6 +140,7 @@ class clTree():
             for row in namedoc:
                 cn = db.connect(self.route)
                 cur = cn.cursor()
+                cur.execute('delete from tbl_LabelPkl where nameFile="%s" and keyLabelPkl="%s"'%(self.namefile,row))
                 cur.execute('INSERT INTO tbl_LabelPkl (nameFile, keyLabelPkl) VALUES ("%s","%s")'%(self.namefile,row))
                 cn.commit()
                 cur.close()
@@ -127,8 +148,18 @@ class clTree():
         else:
             msj = 'no se pudo cargar datos'
         return msj
-    #return list pkl
+    
+    def deleteAllContent(self):
+        cn = db.connect(self.route)
+        cur = cn.cursor()
+        cur.execute('delete from tbl_keyslabel;')
+        cur.execute('delete from tbl_listState;')
+        cur.execute('delete from tbl_LabelPkl;')
+        cn.commit()
+        cur.close()
+    
     def getloadPKL(self):
+        li_1 = []
         cn = db.connect(self.route)
         cur = cn.cursor()
         cur.execute("SELECT keyLabelPkl FROM tbl_LabelPkl where nameFile='%s'"%(self.namefile))
@@ -136,7 +167,118 @@ class clTree():
         cn.commit()
         cur.close()
         if(sc_list):
-            sc_list = sc_list
+            sc_list = [list(i) for i in sc_list]
+            for i in sc_list:
+                li_1.append(i[0])
+            sc_list = li_1
         else:
             sc_list = '!no existen estados asociados para este archivo¡'
-        return  [list(i) for i in sc_list]
+        return  sc_list
+      
+    def searchmodel(self,i_d,state):
+        flag = True 
+        if(i_d != '' and state  != '' ):     
+
+            try:    
+                model = self.tree_config['models']
+                for i in  model:
+                    if(i['id'] == i_d and i['state'] == state):
+                        flag = False
+            except KeyError as err:
+                print('el archivo cargado no tiene la estructura correcta')
+        return flag
+    
+    def makemodel(self,i_d,state):
+        if(self.searchmodel(i_d,state)):
+            self.tree_config['models'].append({'id': i_d, 'state': state})
+            msj = 'Exito al agregar modelo'
+        else:
+            msj = 'Error al crear modelo'
+        return msj
+    
+    def loadTreeConfig(self):
+        resource = {"final_state":0,}
+        if(self.namefile):   
+            try:
+                try:
+            
+                    file =  json.load(open(self.namefile,'r'))
+                    self.tree_config['templates'] = file['templates']
+                    self.tree_config['topics'] = file['topics']
+                    self.tree_config['models'] = file['templates']
+                    self.tree_config['transitions'] = file['transitions']
+                    self.tree_config['initial_state'] = file['initial_state']
+                    self.tree_config['final_state'] = file['final_state']
+                    self.tree_config['max_attempts'] = file['max_attempts']
+                    msj = '!Recursos cargados¡'
+                except KeyError as err:
+                      msj = '!No se pudo cargar archivo\n la estructura del archivo no es compatible¡'
+            
+            except OSError:
+                msj = '!No se pudo cargar archivo, Archivo inexistente¡'
+        else:
+            msj = '!Ingresa un Archivo¡'
+        return msj    
+    def maketemplate(self,i_d,label,state,topic):
+        msj = ''
+        if(self.validateTemplate(i_d,label,state,topic)):
+            self.tree_config['templates'].append({'id': i_d,'label': label,'state': state,'topic': topic})
+            msj = 'Template agregado'
+        else: 
+            msj = 'Error al agregar template'
+        
+        return msj
+
+    
+    def validateTemplate(self,i_d,label,state,topic):
+        flag = True 
+        if(i_d != '' and state  != '' and label != '' ):     
+
+            try:    
+                model = self.tree_config['templates']
+                for i in  model:
+                    if(i['id'] == i_d and i['state'] == state and i['label'] == label and i['topic'] == topic):
+                        flag = False
+            except KeyError as err:
+                print('el archivo cargado no tiene la estructura correcta')
+        return flag
+    
+    def validatetrasition(self,endstate, is_forboo,label,ini_state):
+        flag = True 
+        if(endstate != '' and is_forboo != '' and label != '' and ini_state != ''):    
+            try:    
+                model = self.tree_config['transitions']
+                for i in  model:
+                    if(i['ending_state'] == endstate and i['is_forced'] == is_forboo and i['label'] == label and i['starting_state'] == ini_state):
+                        flag = False
+            except KeyError as err:
+                print('el archivo cargado no tiene la estructura correcta')
+        return flag
+    
+    def maketrasition(self,endstate, is_forboo,label,ini_state):
+        if(self.validatetrasition(endstate, is_forboo,label,ini_state)):    
+            self.tree_config['transitions'].append({'ending_state': endstate, 
+                                           'is_forced': is_forboo, 
+                                           'label': label,
+                                           'starting_state': ini_state})
+            msj = 'Transicion agregada'
+        else:
+            msj = 'Error al agregar template'
+        return msj
+    def makeFile(self,name,final_state, initial_state,max_attempts):
+        self.tree_config['final_state'] = final_state
+        self.tree_config['initial_state'] = initial_state
+        self.tree_config['max_attempts'] = max_attempts
+        if(self.tree_config != {} ):
+            if(name != ''):
+                file = name+'.json'
+                self.name = name+'.json'
+            else:
+                file = self.name
+        else:
+            file = name+'.json'
+            self.name = name+'.json'
+
+        with open(file, 'w', encoding='utf-8') as outfile:
+             json.dump(self.tree_config, outfile)
+        return self.name 
